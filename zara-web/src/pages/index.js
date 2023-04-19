6ae2bac3d96bdd84d53dee3603812fd4fc2e8c56
dogefatherlloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-19 15:47:29 +0000

    update 1

--- zara-web/src/pages/index.js
+++ zara-web/src/pages/index.js
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 export default function Home() {
-  return (
-    <div>hello world</div>
-  )
+  const [apiKey, setApiKey] = useState ('');
+
+return <div className="flex flex-col h-screen">
+  <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
+    <div className="text-xl font-bold">Zara</div>
+    <div>
+      <input 
+      type="password"
+      className="border p-1 rounded" 
+      onChange={e => setApiKey(e.target.value)}
+      value={apiKey}
+      placeholder="Paste API Key here" />
+    </div>
+  </nav>
+  </div>
 }
