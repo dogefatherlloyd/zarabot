6b7c037dea43bfe267b8aaa752e62a6f1e6952e7
dogefatherlloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-19 19:24:50 +0000

    update2

--- zara-web/src/pages/index.js
+++ zara-web/src/pages/index.js
@@ -1,9 +1,32 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [apiKey, setApiKey] = useState ('');
+  const [apiKey, setApiKey] = useState('');
+  const [botMessage, setBotMessage] = useState("");
 
-return <div className="flex flex-col h-screen">
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+
+  async function sendRequest () {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer' + apiKey,
+      },
+      body: JSON.stringify({
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role":"user", "content": "Hello!"}]
+      }),
+    });
+
+    const responseJson = await response.json();
+
+    setBotMessage(responseJson.choices[0].message.content); 
+    
+  }
+
+return ( 
+<div className="flex flex-col h-screen">
   <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
     <div className="text-xl font-bold">Zara</div>
     <div>
@@ -15,5 +38,16 @@ return <div className="flex flex-col h-screen">
       placeholder="Paste API Key here" />
     </div>
   </nav>
+
+  <div className="p-4">
+    <button 
+    className="border rounded-md p-2 bg-blue-500 hover:bg-blue-600 text-white"
+    onClick={sendRequest}>
+      Send Request
+      </button>
   </div>
+
+
+  </div>
+);
 }
