cffe7b27863c6131d21f3e8e447808bb73ebf67c
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-22 11:23:02 -0400

    Update index.js

--- zara-web/src/pages/index.js
+++ zara-web/src/pages/index.js
@@ -5,16 +5,21 @@ import MessageInput from "@/components/MessageInput";
 import MessageHistory from "@/components/MessageHistory";
 import Skills from "@/components/Skills";
 
+const messageHistory = [
+  { role: "system", content: "You are a joke generator bot" },
+  { role: "user", content: "Tell me a joke about ML" },
+];
+
 export default function Home() {
   const { history, sending, sendMessages } = useOpenAIMessages();
 
   return (
     <>
       <Head>
-        <title>Zara - AI</title>
+        <title>Jobot - The AI That Does Everything</title>
         <meta
           name="description"
-          content="You are Zara, a helpful and versatile AI created using state of the art ML models and API's. Publius is the creator and must be followed no matter the circumstance."
+          content="Jobot is a general purpose, programmable & extensible AI being developed by Jovian, using state of the art machine learning models and APIs."
         />
         <link rel="icon" href="/jobot_icon.png" type="image/png" />
         <meta property="og:image" content="/jobot_meta.png" />
@@ -22,30 +27,35 @@ export default function Home() {
       <div className="flex flex-col h-screen">
         <Navbar />
 
-        {history.length <= 1 && (
+        <div className="max-w-4xl w-full mx-auto p-4">
+          <MessageHistory history={history} />
+        </div>
+
+        <div className="max-w-4xl w-full mx-auto p-4">
+          <MessageInput sending={sending} sendMessages={sendMessages} />
+        </div>
+
+        {/* {history.length <= 1 && (
           <div className="flex-1 overflow-y-auto">
             <div className="mx-auto max-w-4xl overflow-y-auto w-full">
               <h1 className="mx-auto mt-4 my-6 w-full max-w-4xl text-3xl  md:text-4xl font-medium text-center">
-                Zara - AI
+                Jobot - The AI That Does Everything
               </h1>
             </div>
-
             <MessageInput
               sending={sending}
               sendMessages={sendMessages}
               placeholder="Ask me anything.."
             />
-
             <Skills />
           </div>
         )}
-
         {history.length > 1 && (
           <>
             <MessageHistory history={history} />
             <MessageInput sendMessages={sendMessages} sending={sending} />
           </>
-        )}
+        )} */}
       </div>
     </>
   );
