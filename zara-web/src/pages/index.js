654add34c86bc8deb358b1d213db020e79fcb2d1
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-22 11:27:46 -0400

    Update index.js

--- zara-web/src/pages/index.js
+++ zara-web/src/pages/index.js
@@ -5,11 +5,6 @@ import MessageInput from "@/components/MessageInput";
 import MessageHistory from "@/components/MessageHistory";
 import Skills from "@/components/Skills";
 
-const messageHistory = [
-  { role: "system", content: "You are a joke generator bot" },
-  { role: "user", content: "Tell me a joke about ML" },
-];
-
 export default function Home() {
   const { history, sending, sendMessages } = useOpenAIMessages();
 
@@ -27,35 +22,30 @@ export default function Home() {
       <div className="flex flex-col h-screen">
         <Navbar />
 
-        <div className="max-w-4xl w-full mx-auto p-4">
-          <MessageHistory history={history} />
-        </div>
-
-        <div className="max-w-4xl w-full mx-auto p-4">
-          <MessageInput sending={sending} sendMessages={sendMessages} />
-        </div>
-
-        {/* {history.length <= 1 && (
+        {history.length <= 1 && (
           <div className="flex-1 overflow-y-auto">
             <div className="mx-auto max-w-4xl overflow-y-auto w-full">
               <h1 className="mx-auto mt-4 my-6 w-full max-w-4xl text-3xl  md:text-4xl font-medium text-center">
                 Jobot - The AI That Does Everything
               </h1>
             </div>
+
             <MessageInput
               sending={sending}
               sendMessages={sendMessages}
               placeholder="Ask me anything.."
             />
+
             <Skills />
           </div>
         )}
+
         {history.length > 1 && (
           <>
             <MessageHistory history={history} />
             <MessageInput sendMessages={sendMessages} sending={sending} />
           </>
-        )} */}
+        )}
       </div>
     </>
   );
