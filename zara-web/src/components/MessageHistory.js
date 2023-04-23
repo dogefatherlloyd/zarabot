0ff46c3af71b12b9d15efd8a7eac6353ae5489de
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-23 11:05:35 -0400

    Update MessageHistory.js

--- zara-web/src/components/MessageHistory.js
+++ zara-web/src/components/MessageHistory.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
-import Image from "next/image"; // Add this import
+import Image from "next/image";
+import { AiOutlineUser, AiOutlineRobot } from "react-icons/ai";
 
 const MessageHistory = ({ history }) => {
   let messagesWindow = useRef();
@@ -10,6 +11,22 @@ const MessageHistory = ({ history }) => {
     }
   }, [history]);
 
+  const getAvatar = (role) => {
+    if (role === "user") {
+      return (
+        <div className="rounded-full bg-blue-400 w-6 h-6 flex items-center justify-center">
+          <AiOutlineUser className="text-white" />
+        </div>
+      );
+    } else {
+      return (
+        <div className="rounded-full bg-red-400 w-6 h-6 flex items-center justify-center">
+          <AiOutlineRobot className="text-white" />
+        </div>
+      );
+    }
+  };
+
   return (
     <div
       className="flex-1 overflow-y-auto py-2 px-2"
@@ -18,18 +35,35 @@ const MessageHistory = ({ history }) => {
       {history
         .filter((message) => message.role !== "system")
         .map((message, index) => (
-          <div key={index} className={message.role === "user" ? "user-message" : "ai-message"}>
-            {message.content.split(/(\[Image: .+?\])/).map((part, i) => {
-              if (/\[Image: .+?\]/.test(part)) {
-                const base64Image = part.replace(/^\[Image: (.+?)\]$/, "$1");
-                return (
-                  <div key={i} className="uploaded-image">
-                    <Image src={base64Image} alt="User uploaded" layout="fill" objectFit="contain" />
-                  </div>
-                );
-              }
-              return <span key={i}>{part}</span>;
-            })}
+          <div key={index} className={message.role === "user" ? "flex justify-end" : "flex"}>
+            <div className="flex items-end">
+              {message.role === "ai" && (
+                <div className="mr-2">{getAvatar(message.role)}</div>
+              )}
+
+              <div className={message.role === "user" ? "text-right" : "text-left"}>
+                {message.content.split(/(\[Image: .+?\])/).map((part, i) => {
+                  if (/\[Image: .+?\]/.test(part)) {
+                    const base64Image = part.replace(/^\[Image: (.+?)\]$/, "$1");
+                    return (
+                      <div key={i} className="uploaded-image">
+                        <Image
+                          src={base64Image}
+                          alt="User uploaded"
+                          layout="fill"
+                          objectFit="contain"
+                        />
+                      </div>
+                    );
+                  }
+                  return <span key={i}>{part}</span>;
+                })}
+              </div>
+
+              {message.role === "user" && (
+                <div className="ml-2">{getAvatar(message.role)}</div>
+              )}
+            </div>
           </div>
         ))}
     </div>
