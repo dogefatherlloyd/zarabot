8436f9aea8febd9dfbcecf9a3b123b28393c818a
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-23 12:07:49 -0400

    Update MessageInput.js

--- zara-web/src/components/MessageInput.js
+++ zara-web/src/components/MessageInput.js
@@ -1,34 +1,29 @@
 import React, { useRef, useEffect } from "react";
+importimport React, { useRef, useEffect } from "react";
 import cn from "classnames";
+import { AiOutlineSend } from "react-icons/ai";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import TextArea from "react-textarea-autosize";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
-import { AiOutlineSend, AiOutlinePaperClip } from "react-icons/ai";
 
 const MessageInput = ({
   sending,
   sendMessages,
-  handleFileUpload,
   placeholder = "Start typing here...",
 }) => {
   const inputRef = useRef(null);
   const [prompt, setPrompt] = useState("");
-
   const handleSendClick = () => {
     if (!prompt) {
       toast.error("Enter a message before you hit send.");
       return;
     }
 
-    sendMessages([{ role: "user", content: prompt }]);
+    sendMessages([{ role: "user", content: prompt }]).then(
+      (success) => !success && setPrompt(prompt)
+    );
     setPrompt("");
-  };
-
-  const handleFileChange = (e) => {
-    if (e.target.files && e.target.files[0]) {
-      handleFileUpload(e.target.files[0]);
-    }
   };
 
   const Icon = sending ? HiOutlineDotsHorizontal : AiOutlineSend;
@@ -61,18 +56,6 @@ const MessageInput = ({
               }
             }}
           />
-          <label htmlFor="file-upload" className="ml-2">
-            <input
-              type="file"
-              accept="image/*"
-              className="hidden"
-              id="file-upload"
-              onChange={handleFileChange}
-            />
-            <button className="rounded-full flex items-center justify-center p-2 bg-blue-500 hover:bg-blue-600 text-white text-lg">
-              <AiOutlinePaperClip style={{ fontSize: 20 }} />
-            </button>
-          </label>
           <button
             className="rounded-full flex items-center justify-center p-2 bg-blue-500 hover:bg-blue-600 text-white text-lg"
             title="Send"
