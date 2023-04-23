90e2bea846cec0f7f2db57cfae3521eb1b31ce3b
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-23 15:27:20 -0400

    Update chat.js

--- zara-web/src/pages/api/chat.js
+++ zara-web/src/pages/api/chat.js
@@ -1,15 +1,28 @@
 import { OpenAIStream } from "@/utils/openai";
 import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { buffer } from "micro";
+import Cors from "micro-cors";
 
-export const config = {
-  runtime: "edge",
-};
+const cors = Cors({
+  allowMethods: ["POST", "OPTIONS"],
+});
 
 async function handler(req, res) {
-  const supabase = createMiddlewareSupabaseClient({ req, res });
-  const body = await req.json();
+  if (req.method === "OPTIONS") {
+    res.status(200).send("");
+    return;
+  }
 
-  body.model = "gpt-3.5-turbo";
+  if (req.method !== "POST") {
+    res.status(404).send("");
+    return;
+  }
+
+  const supabase = createMiddlewareSupabaseClient({ req, res });
+  const body = await buffer(req);
+
+  const jsonBody = JSON.parse(body.toString());
+  jsonBody.model = "davinci-2.0";
 
   const {
     data: { user },
@@ -20,9 +33,44 @@ async function handler(req, res) {
     return new Response("Unauthorized", { status: 401 });
   }
 
-  const stream = await OpenAIStream(body);
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-  return new Response(stream, { status: 200 });
+  const stream = await OpenAIStream(jsonBody);
+
+  const writer = res.writable;
+
+  const encoder = new TextEncoder();
+
+  const streamReader = stream.getReader();
+
+  async function write(message) {
+    const encoded = encoder.encode(message);
+    await writer.write(encoded);
+  }
+
+  async function handleResponse() {
+    try {
+      while (true) {
+        const { done, value } = await streamReader.read();
+        if (done) {
+          await write("data: [DONE]\n\n");
+          return;
+        }
+        const chunk = value.buffer;
+        const text = new TextDecoder("utf-8").decode(chunk);
+        await write(`data: ${JSON.stringify({ text })}\n\n`);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  await handleResponse();
+
+  return res.end();
 }
 
-export default handler;
+export default cors(handler);
