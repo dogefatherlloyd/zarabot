95b8e486d3315ac2a32e8589db915f37ce0e07dc
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-23 15:29:07 -0400

    Update MessageInput.js

--- zara-web/src/components/MessageInput.js
+++ zara-web/src/components/MessageInput.js
@@ -14,6 +14,7 @@ const MessageInput = ({
 }) => {
   const inputRef = useRef(null);
   const [prompt, setPrompt] = useState("");
+  const [filePreview, setFilePreview] = useState("");
 
   const handleSendClick = () => {
     if (!prompt) {
@@ -23,11 +24,13 @@ const MessageInput = ({
 
     sendMessages([{ role: "user", content: prompt }]);
     setPrompt("");
+    setFilePreview("");
   };
 
   const handleFileChange = (e) => {
     if (e.target.files && e.target.files[0]) {
       handleFileUpload(e.target.files[0]);
+      setFilePreview(URL.createObjectURL(e.target.files[0]));
     }
   };
 
@@ -85,6 +88,11 @@ const MessageInput = ({
             />
           </button>
         </div>
+        {filePreview && (
+          <div className="mt-2 flex justify-center">
+            <img src={filePreview} alt="File preview" style={{ maxHeight: 150 }} />
+          </div>
+        )}
       </div>
     </div>
   );
