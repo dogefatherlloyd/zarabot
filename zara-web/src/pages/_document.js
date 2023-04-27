938feec3c42f6b7ddf7ac32230942f527e420adc
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-27 16:26:43 +0000

    4.27.9

--- zara-web/src/pages/_document.js
+++ zara-web/src/pages/_document.js
@@ -1,15 +1,9 @@
-import { Head, Html, Main, NextScript } from "next/document";
-import i18nextConfig from "../next-i18next.config";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document(props) {
-  const currentLocale =
-    props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
+export default function Document() {
   return (
-    <Html lang={currentLocale}>
-      <Head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
-      </Head>
+    <Html lang="en">
+      <Head />
       <body>
         <Main />
         <NextScript />
