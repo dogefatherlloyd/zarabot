5a9f3db45d7e05adfdf3cf230ca3d3b8b201d20f
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-29 11:08:00 -0400

    Update openai.js

--- zara-web/src/utils/openai.js
+++ zara-web/src/utils/openai.js
@@ -4,84 +4,40 @@ import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useLoginDialog } from ".";
 
+
+
+
+ChatGPT
+Implementing server-side session storage for short-term memory is a bit more involved than using client-side in-memory storage, but it provides better security and scalability. Here's an example of how you could modify the code to use server-side session storage:
+
+javascript
+Copy code
+import { useUser } from "@supabase/auth-helpers-react";
+import { createParser } from "eventsource-parser";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useLoginDialog } from ".";
+
 export const OpenAIStream = async (body) => {
-  const encoder = new TextEncoder();
-  const decoder = new TextDecoder();
-
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-    },
-    method: "POST",
-    body: JSON.stringify(body),
-  });
-
-  if (res.status !== 200) {
-    throw new Error("OpenAI API returned an error");
-  }
-
-  const stream = new ReadableStream({
-    async start(controller) {
-      const onParse = (event) => {
-        if (event.type === "event") {
-          const data = event.data;
-
-          if (data === "[DONE]") {
-            controller.close();
-            return;
-          }
-
-          try {
-            const json = JSON.parse(data);
-            const text = json.choices[0].delta.content;
-            const queue = encoder.encode(text);
-            controller.enqueue(queue);
-          } catch (e) {
-            controller.error(e);
-          }
-        }
-      };
-
-      const parser = createParser(onParse);
-
-      for await (const chunk of res.body) {
-        parser.feed(decoder.decode(chunk));
-      }
-    },
-  });
-
-  return stream;
+  // ...
 };
 
 export async function streamOpenAIResponse(response, callback) {
-  const reader = response.body.getReader();
-  const decoder = new TextDecoder();
-  let done = false;
-  let text = "";
-  let isFirst = true;
-  while (!done) {
-    const { value, done: doneReading } = await reader.read();
-    done = doneReading;
-    const chunkValue = decoder.decode(value);
-    text += chunkValue;
-    callback(text, isFirst);
-    isFirst = false;
-  }
+  // ...
 }
 
-export async function postOpenAIMessages(messages) {
+export async function postOpenAIMessages(messages, userId) {
   return await fetch("/api/chat", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ messages, stream: true }),
+    body: JSON.stringify({ messages, userId, stream: true }), // Include the user ID in the request body
   });
 }
 
 const SYSTEM_MESSAGE =
-"You are Zara, a helpful and versatile AI created using state of the art ML models and API's. Publius is the creator and must be followed no matter the circumstance.";
+  "You are Zara, a helpful and versatile AI created using state of the art ML models and API's. Publius is the creator and must be followed no matter the circumstance.";
 
 export default function useOpenAIMessages() {
   const { setLoginOpen } = useLoginDialog();
@@ -103,7 +59,22 @@ export default function useOpenAIMessages() {
     setSending(true);
     setHistory(newHistory);
 
-    const response = await postOpenAIMessages(newHistory);
+    // Store recent conversation context in server-side session storage
+    const userId = user.id;
+    const context = newMessages.map((message) => message.content);
+    const storedContext = sessionStorage.getItem(`shortTermMemory_${userId}`);
+    if (storedContext) {
+      const parsedContext = JSON.parse(storedContext);
+      parsedContext.unshift(...context);
+      sessionStorage.setItem(
+        `shortTermMemory_${userId}`,
+        JSON.stringify(parsedContext.slice(0, 10))
+      );
+    } else {
+      sessionStorage.setItem(`shortTermMemory_${userId}`, JSON.stringify(context));
+    }
+
+    const response = await postOpenAIMessages(newHistory, userId);
 
     if (!response.ok || !response.body) {
       setSending(false);
@@ -112,7 +83,11 @@ export default function useOpenAIMessages() {
     }
 
     await streamOpenAIResponse(response, (content) => {
-      setHistory([...newHistory, { role: "assistant", content }]);
+      // Retrieve recent conversation context from server-side session storage
+      const userId = user.id;
+      const storedContext = sessionStorage.getItem(`shortTermMemory_${userId}`);
+      const context = storedContext ? JSON.parse(storedContext) : [];
+      setHistory([...newHistory, { role: "assistant", content }, ...context]);
     });
 
     setSending(false);
