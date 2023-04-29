a79837d669f3d8ec696e546a775db1f27b7357a0
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-04-28 21:11:12 -0400

    Update TextInput.js

--- zara-web/src/components/inputs/TextInput.js
+++ zara-web/src/components/inputs/TextInput.js
@@ -9,7 +9,7 @@ const TextInput = ({
   <div className="mt-4">
     <label
       htmlFor={field}
-      className="block text-sm font-medium leading-6 text-gray-900 "
+      className="block text-sm font-medium leading-6 text-white"
     >
       {label}
     </label>
