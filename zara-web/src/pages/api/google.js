2c680493b2cf163208663c3463b524cf1b3f4987
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-05-01 13:37:31 +0000

    update 5.1.5

--- zara-web/src/pages/api/google.js
+++ zara-web/src/pages/api/google.js
@@ -1,16 +1,16 @@
-import { OPENAI_API_HOST } from "@/utils/app/const"
-import { cleanSourceText } from "@/utils/server/google"
+import { OPENAI_API_HOST } from "@/utils/app/const";
+import { cleanSourceText } from "@/utils/server/google";
 
-import { Readability } from "@mozilla/readability"
-import endent from "endent"
-import jsdom, { JSDOM } from "jsdom"
+import { Readability } from "@mozilla/readability";
+import endent from "endent";
+import jsdom, { JSDOM } from "jsdom";
 
 const handler = async (req, res) => {
   try {
-    const { messages, key, model, googleAPIKey, googleCSEId } = req.body
+    const { messages, key, model, googleAPIKey, googleCSEId } = req.body;
 
-    const userMessage = messages[messages.length - 1]
-    const query = encodeURIComponent(userMessage.content.trim())
+    const userMessage = messages[messages.length - 1];
+    const query = encodeURIComponent(userMessage.content.trim());
 
     const googleRes = await fetch(
       `https://customsearch.googleapis.com/customsearch/v1?key=${
@@ -18,63 +18,63 @@ const handler = async (req, res) => {
       }&cx=${
         googleCSEId ? googleCSEId : process.env.NEXT_PUBLIC_GOOGLE_CSE_ID
       }&q=${query}&num=5`
-    )
+    );
 
-    const googleData = await googleRes.json()
+    const googleData = await googleRes.json();
 
-    const sources = googleData.items.map(item => ({
+    const sources = googleData.items ? googleData.items.map(item => ({
       title: item.title,
       link: item.link,
       displayLink: item.displayLink,
       snippet: item.snippet,
       image: item.pagemap?.cse_image?.[0]?.src,
       text: ""
-    }))
+    })) : [];
 
     const sourcesWithText = await Promise.all(
       sources.map(async source => {
         try {
           const timeoutPromise = new Promise((_, reject) =>
             setTimeout(() => reject(new Error("Request timed out")), 5000)
-          )
+          );
 
-          const res = await Promise.race([fetch(source.link), timeoutPromise])
+          const res = await Promise.race([fetch(source.link), timeoutPromise]);
 
-          // if (res) {
-          const html = await res.text()
+          if (res) {
+            const html = await res.text();
 
-          const virtualConsole = new jsdom.VirtualConsole()
-          virtualConsole.on("error", error => {
-            if (!error.message.includes("Could not parse CSS stylesheet")) {
-              console.error(error)
-            }
-          })
+            const virtualConsole = new jsdom.VirtualConsole();
+            virtualConsole.on("error", error => {
+              if (!error.message.includes("Could not parse CSS stylesheet")) {
+                console.error(error);
+              }
+            });
 
-          const dom = new JSDOM(html, { virtualConsole })
-          const doc = dom.window.document
-          const parsed = new Readability(doc).parse()
+            const dom = new JSDOM(html, { virtualConsole });
+            const doc = dom.window.document;
+            const parsed = new Readability(doc).parse();
 
-          if (parsed) {
-            let sourceText = cleanSourceText(parsed.textContent)
+            if (parsed) {
+              let sourceText = parsed.textContent ? cleanSourceText(parsed.textContent) : "";
 
-            return {
-              ...source,
+              return {
+                ...source,
 
-              // TODO: switch to tokens
-              text: sourceText.slice(0, 2000)
+                // TODO: switch to tokens
+                text: sourceText.slice(0, 2000)
+              };
             }
           }
-          // }
 
-          return null
+          return null;
         } catch (error) {
-          console.error(error)
-          return null
+          console.error(error);
+          return null;
         }
       })
-    )
+    );
 
-    const filteredSources = sourcesWithText.filter(Boolean)
+    const filteredSources = sourcesWithText.filter(Boolean);
 
     const answerPrompt = endent`
     Provide me with the information I requested. Use the sources to provide an accurate response. Respond in markdown format. Cite the sources you used as a markdown link as you use them at the end of each sentence by number of the source (ex: [[1]](link.com)). Provide an accurate response and then stop. Today's date is ${new Date().toLocaleDateString()}.
@@ -133,9 +133,9 @@ const handler = async (req, res) => {
 
     res.status(200).json({ answer })
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: "Error" })
+    console.error(error);
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
-export default handler
+export default handler;
