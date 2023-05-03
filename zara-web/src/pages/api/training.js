19f2c7a3f0aaca4c06874bb3762c9449c3f07bb3
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-05-03 12:11:33 +0000

    update 5.3.2

--- zara-web/src/pages/api/training.js
+++ zara-web/src/pages/api/training.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         const [{ embedding }] = embeddingResponse.data.data;
 
         // Save to Supabase
-        const { data, error } = await supabase
+        const { error } = await supabase
             .from('documents')
             .insert([
                 { content: text, embedding: embedding }
