8fc2cd499cc3c3aff6a5dd827a8cd54078601de0
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2024-08-27 15:29:36 +0000

    update 8.27.1129

--- zara-web/src/pages/_app.js
+++ zara-web/src/pages/_app.js
@@ -1,12 +1,11 @@
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { useState } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "react-hot-toast";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const supabaseClient = createPagesBrowserClient();
 
   return (
     <>
