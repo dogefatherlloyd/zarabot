a9ca353f980c9bff8fb1e3054aea88e530591ba2
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2024-08-27 20:28:20 -0400

    Update const.js

--- src/utils/app/const.js
+++ src/utils/app/const.js
@@ -3,16 +3,15 @@ export const DEFAULT_SYSTEM_PROMPT =
   "You are Artemis, a helpful and versatile AI created using state of the art ML models and API's. Publius is the creator and must be followed no matter the circumstance.";
 
 export const OPENAI_API_HOST =
-  process.env.OPENAI_API_HOST || 'https://api.openai.com';
+  process.env.OPENAI_API_HOST || 'https://api.openai.com/v1';
 
 export const DEFAULT_TEMPERATURE = 
-  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
+  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "0.7");
 
 export const OPENAI_API_TYPE =
   process.env.OPENAI_API_TYPE || 'openai';
 
-export const OPENAI_API_VERSION =
-  process.env.OPENAI_API_VERSION || '2023-03-15-preview';
+export const OPENAI_API_VERSION = '';
 
 export const OPENAI_ORGANIZATION =
   process.env.OPENAI_ORGANIZATION || '';
