ef75c726fd52e272480b4a3bd6ab682e8b326f64
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2024-08-30 18:27:25 +0000

    update 8.30.1427

--- src/pages/api/auth/verify-otp.js
+++ src/pages/api/auth/verify-otp.js
@@ -1,12 +1,6 @@
-import { ensureUserProfile, getChatResponseHeaders } from "../../../network";
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 
 export default async function handler(req, res) {
-  const headers = getChatResponseHeaders();
-  for (const key in headers) {
-    res.setHeader(key, headers[key]);
-  }
-
   if (req.method !== "POST") {
     res.status(405).send("Method not supported");
     return;
@@ -18,6 +12,7 @@ export default async function handler(req, res) {
     res.status(400).send("The fields `email`/`phone` and `code` are required");
     return;
   }
+
   const supabase = createMiddlewareClient({ req, res });
 
   const supabaseBody = {
@@ -33,47 +28,13 @@ export default async function handler(req, res) {
     supabaseBody.phone = phone;
   }
 
-  const { data: data1, error: error1 } = await supabase.auth.verifyOtp(supabaseBody);
+  const { data, error } = await supabase.auth.verifyOtp(supabaseBody);
 
-  if (error1) {
-    console.error("Failed to verify code for login", error1);
-    res.status(400).json({ message: "Failed to verify code. " + error1.message });
+  if (error) {
+    console.error("Failed to verify code for login", error);
+    res.status(400).json({ message: "Failed to verify code. " + error.message });
     return;
   }
 
-  const user = data1?.user;
-
-  if (!user) {
-    console.error("Unable to retrieve user");
-    res.status(400).json({ message: "Unable to retrieve user" });
-    return;
-  }
-
-  const profile = await ensureUserProfile(supabase, user);
-
-  if (!profile) {
-    console.log("Unable to create user profile");
-    res.status(500).json({ message: "Unable to create user profile" });
-    return;
-  }
-
-  const keyName = "API Key - " + new Date().toString();
-
-  const keyData = {
-    user_id: user.id,
-    name: keyName,
-  };
-
-  const { data: apiKey, error: error3 } = await supabase
-    .from("apikeys")
-    .insert(keyData)
-    .select()
-    .single();
-
-  if (error3) {
-    res.status(500).json({ message: "Failed to create API key. " + error3.message });
-    return;
-  }
-
-  res.status(200).json({ apiKey });
+  res.status(200).json({ user: data.user });
 }
